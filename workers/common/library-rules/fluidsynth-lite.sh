bfb0e2f2a8901ece70538a4527484a53f5223eff
Bastien Bouclet <bastien.bouclet@gmail.com>
2018-11-11 10:32:03 +0100

    Update fluidsynth-lite
    Allows to remove the macos specific patch

--- workers/common/library-rules/fluidsynth-lite.sh
+++ workers/common/library-rules/fluidsynth-lite.sh
@@ -1,8 +1,7 @@
 get_dependencies cmake
 wget -O - --progress=bar:force:noscroll \
-	https://github.com/dotfloat/fluidsynth-lite/archive/cdc8115d4f2d6e8280759bbe514ba15fa88053ea.tar.gz | tar -xzf -
+	https://github.com/Doom64/fluidsynth-lite/archive/a9a3d6b9b1247d143024e092d8607de8c16fdd8a.tar.gz | tar -xzf -
 cd fluidsynth-lite*/
 cmake -DCMAKE_INSTALL_PREFIX=$prefix \
 	-DBUILD_SHARED_LIBS=no .
 do_make
-cp -a src/libfluidsynth.a $prefix/lib
